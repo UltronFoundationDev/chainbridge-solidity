d035507e7ee90c146cc20af6db709b01682e7bef
awowed <software@ultron.foundation>
2022-04-08 06:24:22 +0300

    update(Sergey): finished multisig contract

--- contracts/Multisig.sol
+++ contracts/Multisig.sol
@@ -4,9 +4,8 @@ pragma solidity 0.8.11;
 contract Multisig {
     event InsertingVoter(address indexed _address);
     event RemovingVoter(address indexed _address);
-    event ConfirmingRequest(address indexed sender, uin256 requestId);
-    event Execution(uint256 indexed requestId);
-    event ExecutionFailure(uint256 indexed requestId);
+    event ConfirmingRequest(address indexed sender, uint256 indexed requestId);
+    event RemovingRequest(address indexed sender, uint256 indexed requestId);
 
     // mapping voter id => voter address
     mapping(uint256 => address) private votersIds;
@@ -33,7 +32,7 @@ contract Multisig {
 
     /**
      * @notice Throws if sender is not a voter
-     */
+    */
     modifier onlyVoter() {
         require(voters[msg.sender], "not a voter");
         _;
@@ -42,8 +41,8 @@ contract Multisig {
     /**
      * @notice Throws if address is zero address
      * @param _address the checking address
-     */
-    modifier notNull(address _address) {
+    */
+    modifier notNullAddress(address _address) {
         require(_address != address(0), "zero address");
         _;
     }
@@ -63,6 +62,15 @@ contract Multisig {
     */
     modifier notVoter(address _address) {
         require(!voters[_address], "already a voter");
+        _;
+    }
+
+    /**
+     * @notice Throws if voter request address is zero address
+     * @param voterRequestId the checking voter request Id
+    */
+    modifier voterRequestExists(uint voterRequestId) {
+        require(voterRequests[voterRequestId].candidate != address(0), "voter address is zero");
         _;
     }
 
@@ -96,33 +104,69 @@ contract Multisig {
     }
 
     /**
-     * @notice Returns voter address by id
+     * @notice Throws if active voters count is not empty
+    */
+    modifier isActiveVotersEmpty() {
+        require(activeVotersCount == 0, "Act. voters not empty");
+        _;
+    }
+
+    /**
+     * @notice Returns voter address by id if id != 0
      * @param id the id of voter 
     */
-    function getVoterById(uint256 id) public view returns (address) {
+    function getVoterById(uint256 id) 
+        public 
+        view 
+        returns (address) 
+    {
         return voterIds[id];
     }
 
     /**
-     * @notice Returns the address precense in voters list
+     * @notice Returns the address precense in voters list 
+     * if address != zero address
      * @param _address the checking address 
     */
-    function getVoterStatusByAddress(address _address) public  view returns (bool) {
+    function getVoterStatusByAddress(address _address) 
+        public 
+        view
+        notNullAddress(_address) 
+        returns (bool) 
+    {
         return voters[_address];
     }
 
     /**
      * @notice Returns overall Voters Count
     */
-    function getActiveVotersCount() public view returns(uint256) {
+    function getActiveVotersCount() 
+        public 
+        view 
+        returns(uint256) 
+    {
         return activeVotersCount;
     }
 
     /**
      * @notice Returns voters list counter
     */
-    function getVotersCounter() internal view returns(uint256) {
+    function getVotersCounter() 
+        internal 
+        view 
+        returns(uint256) 
+    {
         return votersCounter;
+    }
+
+    /**
+     * @notice Adds new voter if voter list is empty
+    */
+    function insertInitialVoter()
+        external
+        isActiveVotersEmpty
+    {
+        insertVoter(msg.sender);
     }
 
     /**
@@ -167,7 +211,7 @@ contract Multisig {
      * @param newVoterAddress the address, which should be inserted
     */
     function replaceVoter(address oldVoterAddress, address newVoterAddress)
-        internal
+        external
         notNull(oldVoterAddress)
         isVoter(oldVoterAddress)
         notNull(newVoterAddress)
@@ -178,5 +222,105 @@ contract Multisig {
         votersIds[votersCounter++] = newVoterAddress;
         RemovingVoter(oldVoterAddress);
         InsertingVoter(newVoterAddress);
+    }
+
+    /**
+     * @notice Allows a voter to remove a confirmation for a transaction
+     * if sender is a voter, voter request is confirmed, voter request is not approved  
+     * @param voterRequestId voter request id
+    */ 
+    function insertConfirmation(uint256 voterRequestId)
+        internal
+        onlyVoter
+        notConfirmed(voterRequestId, msg.sender)
+        voterRequestExists(voterRequestId)
+        notExecuted(voterRequestId)
+    {
+        voterConfirmations[voterRequestId][msg.sender] = true;
+        ConfirmingRequest(voterRequestId, msg.sender);
+    }
+
+    /**
+     * @notice Allows a voter to remove a confirmation for a transaction
+     * if sender is a voter, voter request is confirmed, voter request is not approved  
+     * @param voterRequestId voter request id
+    */ 
+    function removeConfirmation(uint256 voterRequestId)
+        internal
+        onlyVoter
+        confirmed(voterRequestId, msg.sender)
+        notExecuted(voterRequestId)
+    {
+        voterConfirmations[voterRequestId][msg.sender] = false;
+        RemovingRequest(msg.sender, voterRequestId);
+    }
+
+    /**
+     * @notice Allows a voter to add a confirmation for a request
+     * if sender is a voter, voter request is confirmed, voter request is not approved  
+     * @param _voters list of voters addresses
+    */
+    function insertVoterRequest(address[] memory _voters) 
+        external 
+        onlyVoter 
+    {
+        for(uint256 i = 0; i < _voters.length; i++) {
+            notVoter(voters[_voters[i]]);
+            voterRequestsCounter = voterRequestsCounter + 1;
+            voterRequests[voterRequestsCounter] = VoterRequest({
+                executed: false,
+                candidate: _voters[i],
+                include: true
+            });
+            insertConfirmation(voterRequestsCounter);
+        }
+    }
+
+    /**
+     * @notice Allows a voter to remove a confirmation for a request
+     * if sender is a voter, voter request is confirmed, voter request is not approved  
+     * @param _voters list of voters addresses
+    */
+    function removeVoterRequest(address[] memory _voters) 
+        external
+        onlyVoter
+    {
+        for(unt256 i = 0; i < _voters.length; i++) {
+            notVoter(voters[_voters[i]]);
+            voterRequestCounter = voterRequestCounter + 1;
+            voterRequests[voterRequestsCounter] = VoterRequest({
+                executed: false,
+                candidate: _voters[i],
+                include: false
+            });
+            insertConfirmation(voterRequestCounter);
+        }
+    }
+
+    /**
+     * @notice Approves voter request if there is enough votes and request is not executed 
+     * @param voterRequestId request is to executed
+    */
+    function votersRequestConclusion(uint256 voterRequestId)
+        external
+        notExecuted(voterRequestId)
+    {
+        uint256 requiredVotesAmount = (activeVoters * 100) / 2;
+        uint256 affirmativeVotesCount = 0;
+        for(uint256 i = 0; i < votersCounter; i++) {
+            if(voterConfirmations[voterRequestId][i] && voters[votersIds[i]]) {
+                    affirmativeVotesCount++;
+            }
+        }
+
+        require(affirmativeVotesCount * 100 > requiredVotesAmount, "not enough votes");
+        if(voterRequests[voterRequestId].include) {
+            insertVoter(voterRequests[voterRequestId].candidate);
+        }
+        else {
+            removeVoter(voterRequests[voterRequestId].candidate);
+        }
+
+        voterRequests[voterRequestId].executed = true;
     }
 }
