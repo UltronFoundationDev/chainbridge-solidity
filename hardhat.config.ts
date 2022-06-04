0ddbf1f711c22ea9c3cebed8e440e5e36c13e04d
ultron <software@ultron.foundation>
2022-06-04 16:44:05 +0300

    update(Sergej): hardhat cfg file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,6 +10,8 @@
  import '@nomiclabs/hardhat-waffle';
  import '@nomiclabs/hardhat-ethers';
 
+ import "./deploy/tasks";
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
@@ -45,15 +47,39 @@ const config: HardhatUserConfig = {
       gas: 2100000,
       gasPrice: 8000000000,
     },
+    ultron: {
+      url: `http://ultron-rpc.net`,
+      chainId: 1231,
+    },
+    ethereum: {
+      url: "https://mainnet.infura.io/v3/",
+      chainId: 1
+    },
+    fantom: {
+      url: "https://rpc.ftm.tools/",
+      chainId: 250
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56
+    },
+    polygon: {
+      url: "https://rpc-mainnet.matic.network",
+      chainId: 137
+    },
+    avalanche: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      chainId: 43114
+    },
+    ultron_testnet: {
+      url: `http://51.250.34.31:18545`,
+      chainId: 1230,
+    },
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       chainId: 5,
       gas: 2100000,
       gasPrice: 8000000000,
-    },
-    ultron: {
-      url: `http://51.250.34.31:18545`,
-      chainId: 1230,
     },
     bsc_testnet: {
       url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
@@ -62,7 +88,15 @@ const config: HardhatUserConfig = {
     fantom_testnet: {
       url: `https://rpc.testnet.fantom.network/`,
       chainId: 4002,
-    }
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today",
+      chainId: 80001
+    }, 
+    avalanche_fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113
+    }, 
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
